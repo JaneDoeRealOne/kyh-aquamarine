255ff245b0329991dcfe3a1b07e3ce55300b7e2e
jbrorson <josefine.brorson@gmail.com>
2021-02-11 11:45:50 +0100

    FAQ expandable list

---
+++ src/components/FaqData.js
@@ -0,0 +1,18 @@
+export const FaqData = [
+  {
+    question: 'What is a polytechnic school?',
+    answer: 'A school where you go to educate yourself, learning by doing'
+  },
+  {
+    question: 'How do I login to my school email?',
+    answer: 'You had a code on text message and on your private email. There you will find an email from KYH with login information. Use the username and password to login.'
+  },
+  {
+    question: 'How do I get paid for my education?',
+    answer: 'You will have to send a request to CSN.'
+  },
+  {
+    question: 'What is a "studieförsäkran"?',
+    answer: 'It´s an insurance CSN asks for to make sure that you are a student in order to get the payment from CSN.'
+  }
+];
---
+++ src/components/FaqList.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+// import '../App.css';
+import { FaqData } from './FaqData';
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+import './FaqList.css';
+
+const FaqList = () => {
+  const [clicked, setClicked] = useState(false)
+
+  const toggle = index => {
+    if (clicked === index) {
+      return setClicked(null)
+    }
+    setClicked(index)
+  }
+
+  return (
+    <IconContext.Provider value={{ color: 'orange', size: '25px' }}>
+      <div className='accordion-wrapper'>
+        Frequently asked questions
+        <div className='accordion-container'>
+          {FaqData.map((item, index) => {
+            return (
+              <>
+                <div className='wrap' onClick={() => toggle(index)} key={index}>
+                  <h1>{item.question}</h1>
+                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+                </div>
+                {clicked === index ? (
+                  <div className='dropdown'>
+                    <p>{item.answer}</p>
+                  </div>
+                ) : null}
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </IconContext.Provider>
+  );
+};
+
+export default FaqList;
