922408ca4ff52a95c5e2b5e900f7e44bf6abaf15
jbrorson <josefine.brorson@gmail.com>
2021-02-16 14:31:14 +0100

    Removed image in hero and added ul for education list

--- src/components/EducationCards.js
+++ src/components/EducationCards.js
@@ -23,6 +23,26 @@ function EducationCards() {
               path='/education'
             />
           </ul>
+          {/* <ul className='cards__items'>
+            <Education
+              src='images/iot-dev.jpg'
+              text='Har du stort intresse för uppkopplade smarta prylar? Missa då inte vår utbildning Internet Of Things'
+              label='Internet of Things'
+              path='/education'
+            />
+            <Education
+              src='images/produktionsledare.jpg'
+              text='Är du intresserad av hur en vanlig dag ser ut för en produktionsledare?'
+              label='Produktionsledare'
+              path='/education'
+            />
+            <Education
+              src='images/iot-dev.jpg'
+              text='Har du stort intresse för uppkopplade smarta prylar? Missa då inte vår utbildning Internet Of Things'
+              label='Internet of Things'
+              path='/education'
+            />
+          </ul> */}
         </div>
       </div>
     </div>
