6bbdb6ae9f31db432edb155da4cd1b3e0effa50e
jbrorson <josefine.brorson@gmail.com>
2021-02-17 15:54:22 +0100

    Implemented React-Select for dropdown

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import '../../App.css';
+import Dropdown from '../Dropdown';
 
 export default function Login() {
   return (
-    <h1 className='login'>Please, log in</h1>
+    <>
+      <h1 className='login'>Please, log in</h1>
+      <Dropdown />
+    </>
   )
 }
