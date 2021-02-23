389aae0b0c397246d150109a2a36a6b3e59690ec
jbrorson <josefine.brorson@gmail.com>
2021-02-23 17:29:46 +0100

    New form style

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,14 +1,17 @@
 import React from 'react';
-// import '../../App.css';
 import '../Login.css';
+import DarkLogo from '../assets/KYH-dark-logo.png';
 // import { Button } from '../Button';
+// import '../../App.css';
 
 export default function Login() {
   return (
     <>
       <section className='login'>
-        {/* <h1 className='form--heading'>Vänligen fyll i dina inloggningsuppgifter</h1> */}
         <div className='login--container'>
+          <div className='logo--container'>
+            <img src={DarkLogo} className='form--logo' alt='logotype' />
+          </div>
           <h1 className='form--h1'>Logga in</h1>
           <p>Email</p>
           <input type='email' label='email' placeholder='Ange din email'></input>
@@ -16,7 +19,7 @@ export default function Login() {
           <input type='password' label='password' placeholder='Ange ditt lösenord'></input>
           {/* <Button className='btn--outline'>Logga in</Button> */}
           <button className='form--btn'>Logga in</button>
-          <p className='form--link'>Glömt lösenord?</p>
+          <a href='/' className='form--link'>Glömt lösenord?</a>
         </div>
       </section>
     </>
