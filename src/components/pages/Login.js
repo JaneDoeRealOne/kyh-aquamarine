bf5a90e0c5ee1b3458148edf5759c6a3b0f23477
jbrorson <josefine.brorson@gmail.com>
2021-02-23 16:42:50 +0100

    Login form and Firebase config

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import '../../App.css';
+// import '../../App.css';
+import '../Login.css';
+// import { Button } from '../Button';
 
 export default function Login() {
   return (
-    <h1 className='login'>Vänligen, logga in</h1>
+    <>
+      <section className='login'>
+        {/* <h1 className='form--heading'>Vänligen fyll i dina inloggningsuppgifter</h1> */}
+        <div className='login--container'>
+          <h1 className='form--h1'>Logga in</h1>
+          <p>Email</p>
+          <input type='email' label='email' placeholder='Ange din email'></input>
+          <p>Lösenord</p>
+          <input type='password' label='password' placeholder='Ange ditt lösenord'></input>
+          {/* <Button className='btn--outline'>Logga in</Button> */}
+          <button className='form--btn'>Logga in</button>
+          <p className='form--link'>Glömt lösenord?</p>
+        </div>
+      </section>
+    </>
   )
 }
